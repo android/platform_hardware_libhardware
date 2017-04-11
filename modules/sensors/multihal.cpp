dc37c100fe62e585ce458de1a1370258e9c25e02
Mark Salyzyn <salyzyn@google.com>
2017-04-11 08:56:09 -0700

    libhardware: deprecate cutils/log.h
    Replace cutils/log.h with log/log.h. Sort headers. Minimize conflicts.
    
    Test: compile
    Bug: 34250038
    Change-Id: I5c0f231cbfed90b73e0538c9d32ad3226c7cc5e4

--- modules/sensors/multihal.cpp
+++ modules/sensors/multihal.cpp
@@ -13,31 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#define LOG_NDEBUG 1
+
+#include <log/log.h>
+#include <cutils/atomic.h>
 
 #include <hardware/sensors.h>
-#include <fcntl.h>
-#include <errno.h>
+
 #include <dirent.h>
+#include <dlfcn.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
 #include <math.h>
 #include <poll.h>
 #include <pthread.h>
-#include <cutils/atomic.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define LOG_NDEBUG 1
-#include <cutils/log.h>
-
-#include <vector>
-#include <string>
 #include <fstream>
 #include <map>
 #include <string>
+#include <vector>
 
-#include <stdio.h>
-#include <dlfcn.h>
 #include <SensorEventQueue.h>
-
-#include <limits.h>
-#include <stdlib.h>
 
 static const char* CONFIG_FILENAME = "/system/etc/sensors/hals.conf";
 static const int MAX_CONF_LINE_LENGTH = 1024;
