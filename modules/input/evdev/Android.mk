a4189002e09cdae18b8bff59c6fe55076abc423f
Lennart Wieboldt <lennart.1997@gmx.de>
2017-07-25 21:10:12 +0200

    Remove LOCAL_CLANG
    clang is the default compiler since Android nougat
    
    Change-Id: If6e62c82e8f64ba3a6c45a455564b16e2e333804
    Signed-off-by: Lennart Wieboldt <lennart.1997@gmx.de>

--- modules/input/evdev/Android.mk
+++ modules/input/evdev/Android.mk
@@ -32,8 +32,8 @@ LOCAL_SHARED_LIBRARIES := \
     liblog \
     libutils
 
-LOCAL_CLANG := true
-LOCAL_CPPFLAGS += -Wno-unused-parameter
+
+LOCAL_CPPFLAGS += -std=c++14 -Wno-unused-parameter
 
 LOCAL_MODULE := libinput_evdev
 LOCAL_MODULE_TAGS := optional
@@ -54,7 +54,6 @@ LOCAL_SHARED_LIBRARIES := \
     libinput_evdev \
     liblog
 
-LOCAL_CLANG := true
 LOCAL_CPPFLAGS += -Wno-unused-parameter
 
 LOCAL_MODULE_TAGS := optional
