c996c19e01160e0382241241f8997f341a50900b
aw691190716 <aw691190716@gmail.com>
2017-09-17 12:42:14 +0800

    add for mango

--- include/hardware/hwcomposer.h
+++ include/hardware/hwcomposer.h
@@ -286,6 +286,11 @@ typedef struct hwc_layer_1 {
              * their origin is the top-left corner.
              */
             hwc_region_t surfaceDamage;
+
+#ifdef QTI_BSP
+            /* Color for Dim Layer */
+            hwc_color_t color;
+#endif
         };
     };
 
