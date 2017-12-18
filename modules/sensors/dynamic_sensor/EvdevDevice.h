7721b28d535d1848508865c789e68af28a70a6b5
Kim Low <klow@gaikai.com>
2017-12-18 10:56:43 -0800

    Add evdev based dynamic sensors
    Similar to HidRaw based dynamic sensors, evdev sensors are added
    to the dynamic sensors framework.
    
    Only absolute axis accelerometer and gyroscope are supported
    for now.
    
    Test: Connect a DualShock4, get the accelerometer/gyroscope,
          and check the data.
    
          Example test code:
              List<Sensor> accSensors = mSensorManager.getDynamicSensorList(Sensor.TYPE_ACCELEROMETER);
              for(int i=0; i<accSensors.size(); i++) {
                  if (accSensors.get(i).isDynamicSensor()) {
                      // Check that it's a DualShock4 with accSensors.get(i).getProduct()
                      mSensorManager.registerListener(this,
                              accSensors.get(i), SensorManager.SENSOR_DELAY_NORMAL);
                  }
              }
              List<Sensor> gyroSensors = mSensorManager.getDynamicSensorList(Sensor.TYPE_GYROSCOPE);
              for(int i=0; i<gyroSensors.size(); i++) {
                  if (gyroSensors.get(i).isDynamicSensor()) {
                      // Check that it's a DualShock4 with gyroSensors.get(i).getProduct()
                      mSensorManager.registerListener(this,
                              gyroSensors.get(i), SensorManager.SENSOR_DELAY_NORMAL);
                  }
              }
    
              @Override
              public final void onSensorChanged(SensorEvent event) {
                  float dT = (event.timestamp - timestamp) / 1000000000.0f;
                  timestamp = event.timestamp;
    
                  if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
                      Log.i(getClass().getName(), "ACCEL: "
                              + event.values[0] + " " + event.values[1] + " " + event.values[2]);
                  else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE)
                      Log.i(getClass().getName(), "GYRO:  "
                              + event.values[0] + " " + event.values[1] + " " + event.values[2]);
              }
    
    Bug: 70356237
    Change-Id: Ib78b1530ca53a1465efaf1de084ea76517fa7451

---
+++ modules/sensors/dynamic_sensor/EvdevDevice.h
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef ANDROID_SENSORHAL_EXT_EVDEV_DEVICE_H
+#define ANDROID_SENSORHAL_EXT_EVDEV_DEVICE_H
+
+#include "Utils.h"
+
+#include <linux/input.h>
+#include <string>
+#include <unordered_set>
+#include <utils/Log.h>
+#include <vector>
+
+namespace android {
+namespace SensorHalExt {
+
+/*
+ * Evdev (event input device) sensors send its sensor data using specific
+ * event types (e.g. EV_ABS) and event codes (e.g. ABS_X, ABS_Y, ABS_Z).
+ * For example, evdev accelerometers send its X axis acceleration using
+ * event code ABS_X, and evdev gyroscopes send its X axis rotation using
+ * event code ABS_RX.
+ *
+ * Please refer to bionic/libc/kernel/uapi/linux/input-event-codes.h for all
+ * event types and codes.
+ *
+ * For more information about evdev devices, please refer to
+ * https://www.kernel.org/doc/Documentation/input/event-codes.txt.
+ */
+
+namespace EvdevSensorTypeUsage {
+enum {
+    ACCELEROMETER_3D = 0x200073,
+    GYROMETER_3D = 0x200076,
+};
+} // namespace EvdevSensorTypeUsage
+
+namespace EvdevSensorAxis {
+enum {
+    ACCELERATION_X_AXIS = ABS_X,
+    ACCELERATION_Y_AXIS = ABS_Y,
+    ACCELERATION_Z_AXIS = ABS_Z,
+    ANGULAR_VELOCITY_X_AXIS = ABS_RX,
+    ANGULAR_VELOCITY_Y_AXIS = ABS_RY,
+    ANGULAR_VELOCITY_Z_AXIS = ABS_RZ,
+};
+} // namespace EvdevSensorAxis
+
+class EvdevDevice : virtual public REF_BASE(EvdevDevice) {
+    friend class EvdevDeviceTest;
+public:
+    EvdevDevice(const std::string &devName, const std::unordered_set<uint32_t> &usageSet);
+
+    ~EvdevDevice();
+
+    struct EvdevDeviceInfo {
+        std::string name;
+        std::string physicalPath;
+        std::string uniqueId;
+        std::string busType;
+        uint16_t vendor;
+        uint16_t product;
+        uint16_t version;
+        uint8_t propBitmask[(INPUT_PROP_MAX + 1) / 8];
+        uint8_t absBitmask[(ABS_MAX + 1) / 8];
+    };
+
+    const EvdevDeviceInfo& getDeviceInfo() { return mDeviceInfo; }
+
+    bool getAbsoluteAxisInfo(uint8_t axis, input_absinfo* outAxisInfo) const;
+
+    // receive from default input endpoint
+    int hasEvent(uint16_t timeout);
+    int receiveEvent(struct input_event *event, size_t eventSize);
+
+    // test if the device initialized successfully
+    bool isValid();
+
+protected:
+    bool populateDeviceInfo();
+    bool generateDigest(const std::unordered_set<uint32_t> &usage);
+
+    std::vector<unsigned int> mDigestVector;
+private:
+    int mDevFd;
+    std::string mDevName;
+    EvdevDeviceInfo mDeviceInfo;
+    int mValid;
+
+    EvdevDevice(const EvdevDevice &) = delete;
+    void operator=(const EvdevDevice &) = delete;
+};
+
+} // namespace SensorHalExt
+} // namespace android
+
+#endif // ANDROID_SENSORHAL_EXT_EVDEV_DEVICE_H
